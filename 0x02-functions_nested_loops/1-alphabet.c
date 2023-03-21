babd6df3c8a23c2750abd9c35fe8bf91c87405c9
mburu-design <mburukennedy67@gmail.com>
2023-03-21 12:23:02 -0700

    ~

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-*print_alphabet - function that prints the alphabet, in lowercase, followed by a new line.
+*print_alphabet - function that prints the alphabet,
+*in lowercase, followed by a new line.
 */
 void print_alphabet(void)
 {
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /** 
 *print_alphabet_x10 - prints the alphabet in lowercase ten times
-*
-* Return: void
+*Return: void
 */
 void print_alphabet_x10(void)
 {
