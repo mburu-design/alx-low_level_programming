1ddd59328a91ceac89a8f01270939f5961f6d94c
mburu-design <mburukennedy67@gmail.com>
2023-03-21 10:52:47 -0700

    ~

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,27 +1,11 @@
 #include "main.h"
 /**
-*print_alphabet fuction
-*prints the alphabet
-*
-*/
-void print_alphabet(void)
-{
-char c = 'a';
-while (c <= 'z')
-{
-_putchar(c);
-c++;
-}
-_putchar('\n');
-}
-
-/**
-* main - check the code
-*
+*main - check the code for Holberton School students*
 * Return: Always 0.
 */
+
 int main(void)
 {
 print_alphabet();
- return (0);
+return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,14 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_alphabet(void);
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
 #endif /* MAIN_H */
