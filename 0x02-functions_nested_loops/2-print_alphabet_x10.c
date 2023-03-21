adcbcc48bbac51466cca6b895d9a02f46ee57d52
mburu-design <mburukennedy67@gmail.com>
2023-03-21 11:48:22 -0700

    ~

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,5 @@
 #include "main.h"
-
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet_x10();
-    return (0);
-
-}
-
-/**
- * print_alphabet_x10 - prints the alphabet in lowercase ten times
+/** print_alphabet_x10 - prints the alphabet in lowercase ten times
  *
  * Return: void
  */
